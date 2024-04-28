8c0a781db5112ac26bb6d005a3d89013cab56a91
Gloridust <Gloridust@gmail.com>
2024-04-28 15:18:16 +0800

    Update c2.c

--- 24-04-28/c2.c
+++ 24-04-28/c2.c
@@ -9,9 +9,5 @@ int main(){
     for(i=n-1;i>=0;i--)
     printf("%f ",a[i]);
     printf("\n");
-    // int main(){
-    // int i=9,a[10]={0,1,2,3,4,5,6,7,8,9};
-    // for(;i>=0;i--)
-    // printf("%d \n",a[i]);
     return 0;
 }
