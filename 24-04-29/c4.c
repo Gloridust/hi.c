93e8f16433c18b6d5b78de2e0fb1fcc2a4545692
Gloridust <gloridust@gmail.com>
2024-04-29 16:04:55 +0800

    c4

--- 24-04-29/c4.c
+++ 24-04-29/c4.c
@@ -3,13 +3,26 @@
 
 int main(){
     char s[12]={'I',' ','L','O','V','E',' ','C','H','I','N','A'};
-    int i;
+    int i,x;
     for ( i = 0; i < 12; i++)
     {
         printf("%c",s[i]);
     }
     printf("\n");
-    printf("%s",s);
+    printf("%s\n",s);
+    printf("The length of s is:%ld",strlen(s));
+    printf("\n");
+
+    for ( i = 0; i < strlen(s); i++)
+    {
+        if (s[i]!=' ')
+        {
+            x++;
+        }
+        
+    }
+    x=(strlen(s)-x);
+    printf("The length of s without space is:%ld",x);
     printf("\n");
 
     return 0;
