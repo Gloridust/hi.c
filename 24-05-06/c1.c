0176363fa89c1c7ef9e091bd63453a0bbe327a2d
Gloridust <gloridust@gmail.com>
2024-05-06 14:47:30 +0800

    c1

---
+++ 24-05-06/c1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[256],b[256];
+    printf("Please input the string:");
+    gets(a);
+    // scanf("%d ",&a);
+    printf("%s\n",a);
+    return 0;
+}
