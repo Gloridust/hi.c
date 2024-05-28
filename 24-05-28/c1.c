a905e8e036e904d020d1ca69755814bd1ef78030
Gloridust <Gloridust@gmail.com>
2024-05-28 19:30:51 +0800

    finished:c1

--- 24-05-28/c1.c
+++ 24-05-28/c1.c
@@ -1,41 +1,35 @@
 // 输入一个具有n个数据元素的一维数组
 // 找出最小的数据元素
-// 用函数实现。
+// 用函数实现
 
 #include <stdio.h>
 
-float input_nums(n){
-    float nums[n],j;
+void input_nums(int n, float nums[]) {
     int i;
-    for (i = 0; i < n; i++)
-    {
-        printf("please input the %dth num:",i);
-        scanf("%f",&j);
-        nums[i]=j;
+    for (i = 0; i < n; i++) {
+        printf("please input the %dth num:", i + 1);
+        scanf("%f", &nums[i]);
     }
-    return nums;
 }
 
-float find_min(n,nums){
+float find_min(int n, float nums[]) {
+    float min = nums[0];
     int i;
-    float min;
-    for (i = 0; i < n; i++)
-    {
-        if (min<nums[i])
-        {
-            min=nums[i];
+    for (i = 1; i < n; i++) {
+        if (nums[i] < min) {
+            min = nums[i];
         }
     }
     return min;
 }
 
-int main(){
+int main() {
     int n;
     printf("please input n:");
-    scanf("%d",&n);
-    float nums[],min_num;
-    nums[]=input_nums;
-    min_num=(n,nums);
- 
+    scanf("%d", &n);
+    float nums[n];
+    input_nums(n, nums);
+    float min_num = find_min(n, nums);
+    printf("The min number is: %f\n", min_num);
     return 0;
 }
